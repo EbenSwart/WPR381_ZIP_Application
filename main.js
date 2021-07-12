1902068b6c68923e43440d1b5d6a4f91ab725581
EbenSwart <79501317+EbenSwart@users.noreply.github.com>
2021-07-12 21:06:22 +0200

    Added compress and extract functions

--- main.js
+++ main.js
@@ -1,75 +1,59 @@
-// let procedure = "";
-// let FilePath = "";
+const decompress = require('decompress');
+var AdmZip = require('adm-zip');
 
-// const readLine = require('readline');
-// const reader = readLine.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
+let procedure = "";
+let FilePath = "";
 
-// reader.question(
-//     "Please choose a function to do (Enter only the number): \n 1. Extract"+
-//     "\n 2. Compress \n Your choice: ", (answer) => {
-//         procedure = answer;  
-//         if (procedure === "1") {
-//             Extract();
-//         }else if(procedure === "2"){
-//             Compress();
-//         }else{
-//             Error();
-//         }
-//     });
+const readLine = require('readline');
+const reader = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-//     function Error(){
-//         console.log("Please select an vallid option!");
-//     }
-
-//     function Compress() {
-//         reader.question("Please enter the filepath\n", (getFilePath) => {
-//             FilePath = getFilePath;
-//             PrintFile();
-//         })
-//     }
-
-//     function Extract() {
-//         reader.question("Please enter the filepath\n", (getFilePath) => {
-//             FilePath = getFilePath;
-//             PrintFile();
-//         })
-//     }
-
-//     function PrintFile(){
-//         console.log("The file path is: " + FilePath);
-//     }
-
-    //#############################################################################################
-
-    var file_system = require('fs');
-    var archiver = require('archiver');
-    var path = require('path');
-    
-    //var output = file_system.createWriteStream('Test.zip');
-    var output = path("D:\\Documents").createWriteStream('Test.zip');
-    var archive = archiver('zip');
-    
-
-
-    output.on('close', function () {
-        console.log(archive.pointer() + ' total bytes');
-        console.log('archiver has been finalized and the output file descriptor has closed.');
+reader.question(
+    "Please choose a function to do (Enter only the number): \n 1. Extract"+
+    "\n 2. Compress \n Your choice: ", (answer) => {
+        procedure = answer;  
+        if (procedure === "1") {
+            Extract();
+        }else if(procedure === "2"){
+            Compress();
+        }else{
+            Error();
+        }
     });
-    
-    archive.on('error', function(err){
-        throw err;
-    });
-    
-    archive.pipe(output);
-    
-    // append files from a sub-directory, putting its contents at the root of archive
-    //archive.directory(source_dir, false);
-    
-    // append files from a sub-directory and naming it `new-subdir` within the archive
-    //archive.directory('subdir/', 'new-subdir');
-    archive.directory("D:\Documents\ZipThisFolder","D:\\Documents");
-    
-    archive.finalize();
+
+    function Error(){
+        console.log("Please select an vallid option!");
+    }
+
+    function Compress() {
+        reader.question("Please enter the filepath\n", (getFilePath) => {
+            FilePath = getFilePath;
+            Zip(FilePath);
+        })
+    }
+
+    function Extract() {
+        reader.question("Please enter the filepath\n", (getFilePath) => {
+            FilePath = getFilePath;
+            Unzip(FilePath);
+        })
+    }
+
+    // ################ Unzip Folder ######################
+    function Unzip(FilePath){
+        (async () => {
+            const files = await decompress("D:\\Documents\\ZipThisFolder.zip", "D:\\Documents\\DecompressedFolder\\");
+            console.log("File was extracted");
+        })();
+    }
+     //################ Unzip Folder ######################
+     //############### Zip Folder #########################
+     function Zip(FilePath){
+        var zip = new AdmZip();
+        zip.addLocalFolder(FilePath);
+        zip.writeZip("D:\\Documents\\CompressedFolder\\Zipped.zip");
+        console.log("File was compressed");
+     }
+     //############### Zip Folder #########################
