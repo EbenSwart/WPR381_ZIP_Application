0280a42988ead4a9ffcdf5cf4fea23db3edaffb7
EbenSwart <79501317+EbenSwart@users.noreply.github.com>
2021-07-13 16:29:00 +0200

    Added comments to make it  easier to understand

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
+//We need these two modules to zip and unzip a folder/file
 const decompress = require('decompress');
 var AdmZip = require('adm-zip');
 
+//this is all the variables that are used in the program
 let procedure = "";
 let SourcePath = "";
 let TargetPath = "";
 let toThisProcedure = 0;
 
-
+//Need this in order to read input from console
 const readLine = require('readline');
 const { exit } = require('process');
 const reader = readLine.createInterface({
@@ -14,6 +16,7 @@ const reader = readLine.createInterface({
     output: process.stdout
 });
 
+//Lets user choose what they want to do
 reader.question(
     "Please choose a function to do (Enter only the number): \n 1. Extract"+
     "\n 2. Compress \n Your choice: ", (answer) => {
@@ -27,11 +30,12 @@ reader.question(
         }
     });
 
+    //When user chooses incorrect option display an error message
     function Error(){
         console.log("Please select an vallid option!");
     }
 
-
+    //Gets the source of the file
     async function GetSource(){
         reader.question("Enter Source:", (sPath) => {
             SourcePath = sPath;
@@ -39,6 +43,7 @@ reader.question(
         })
     }
 
+    //Gets the target: where the file should be saved
     async function GetTarget(){
         reader.question("Target:", (tPath) => {
             TargetPath = tPath;
@@ -46,23 +51,23 @@ reader.question(
                 Zip(SourcePath, TargetPath);
             }else if (toThisProcedure == 1){
                 Unzip(SourcePath, TargetPath);
-            }else{
-                Error();
             }
         })
     }
 
+    //This call the functions required to zip a folder/file
     function Compress(){
         toThisProcedure = 2;
         GetSource().then();      
     }
 
+    //This call the functions required to UnZip a folder/file
     function Extract(){
         toThisProcedure = 1;
         GetSource().then();      
     }
 
-    // ################ Unzip Folder ######################
+    //UnZips the source direcotory and saves it at target
     async function Unzip(Source, Target){
         (async () => {
             const files = await decompress(Source, Target);
@@ -70,9 +75,8 @@ reader.question(
             exit(0);
         })();
     }
-     //################ Unzip Folder ######################
-
-     //############### Zip Folder #########################
+    
+     //Zips the source direcotory and saves it at target
      async function Zip(Source, Target){
         var zip = new AdmZip();
         zip.addLocalFolder(Source);
@@ -80,4 +84,4 @@ reader.question(
         console.log("File was compressed");
         exit(0);
      }
-     //############### Zip Folder #########################
+
